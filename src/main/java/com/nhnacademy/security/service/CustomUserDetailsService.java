4abc3bad9176ee2c7eebb416bbd37960db02575e
신동민/NE <dongmyo@nhnent.com>
2024-02-26 04:09:59 +0900

    * custom userdetailsservice 정답

--- src/main/java/com/nhnacademy/security/service/CustomUserDetailsService.java
+++ src/main/java/com/nhnacademy/security/service/CustomUserDetailsService.java
@@ -1,9 +1,17 @@
 package com.nhnacademy.security.service;
 
+import com.nhnacademy.security.entity.Member;
 import com.nhnacademy.security.repository.MemberRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Objects;
 
 public class CustomUserDetailsService implements UserDetailsService {
     private final MemberRepository memberRepository;
@@ -16,7 +24,15 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // TODO #5: 실습 - Custom UserDetailsService 구현
-        return null;
+        Member member = memberRepository.findByName(username);
+        if (Objects.isNull(member)) {
+            throw new UsernameNotFoundException(username + " not found");
+        }
+
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().getAuthority());
+        Collection<GrantedAuthority> grantedAuthorities = Collections.singletonList(grantedAuthority);
+
+        return new User(member.getName(), member.getPassword(), grantedAuthorities);
     }
 
 }
