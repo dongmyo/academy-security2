22e509a49b62d80d92de9e35c8ab4038051cf745
신동민/NE <dongmyo@nhnent.com>
2024-02-27 01:20:01 +0900

    * method security 실습 정답

--- src/main/java/com/nhnacademy/security/service/AdminOrMemberService.java
+++ src/main/java/com/nhnacademy/security/service/AdminOrMemberService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AdminOrMemberService {
     // TODO #2: 실습 - 어드민이나 멤버만 이 메서드를 실행할 수 있도록 설정하세요.
-    /* ... */
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN', 'ROLE_MEMBER')")
     public String getLinkOnlyAdminOrMemberCanAccess() {
         return "https://www.youtube.com/watch?v=otCpCn0l4Wo";
     }
